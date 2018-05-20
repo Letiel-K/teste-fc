922fb5bf00237676a8392bbd7bc224d9d2c04d62
Letiel-K <letiel@gmail.com>
2018-05-20 14:46:50 -0300

    Update config-banco-dados.php

--- src/model/config-banco-dados.php
+++ src/model/config-banco-dados.php
@@ -2,5 +2,5 @@
 
 	define("DB_HOST", "localhost");
 	define("DB_NAME", "teste_fc");
-	define("DB_USER", "letiel");
-	define("DB_PASS", "letiel");
+	define("DB_USER", "root");
+	define("DB_PASS", "");
